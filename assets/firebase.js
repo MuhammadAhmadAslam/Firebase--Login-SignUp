b1a705375e5d8591b054cb14f5cfec622cfd9566
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-08-05 19:17:27 +0500

    Commit

--- assets/firebase.js
+++ assets/firebase.js
@@ -13,17 +13,8 @@
  const auth = getAuth(app);
 
 
- createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log(user);
-    
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+export {
+  auth , 
+  createUserWithEmailAndPassword,
+}
  
--- assets/signup.js
+++ assets/signup.js
@@ -25,3 +25,58 @@ link.addEventListener("click", e => {
  forms.classList.toggle("show-signup");
 })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {auth , createUserWithEmailAndPassword} from "./firebase.js"
+
+
+
+function signUpFunction(){
+  const toast = document.querySelector('.toast')
+  const signupEmail = document.getElementById('signup-email')
+  const signupPassword = document.getElementById('signup-password')
+  createUserWithEmailAndPassword(auth, signupEmail.value, signupPassword.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    console.log('signup successful');
+    Swal.fire({
+      title: "Good job!",
+      text: "You have sucessfully created account",
+      icon: "success"
+    });
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: errorCode,
+    });
+  });
+}
+
+let signupbtn = document.getElementById('signupbtn')
+console.log(signupbtn);
+
+signupbtn.addEventListener('click' , signUpFunction)
